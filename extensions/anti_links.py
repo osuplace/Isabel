cdceb69913be0db1117a496e1c3e866c1a8cfd45
Endrik Tombak <littleendu@gmail.com>
2024-04-03 21:25:03 +0300

    better anti scam measure

--- extensions/anti_links.py
+++ extensions/anti_links.py
@@ -8,6 +8,7 @@ if TYPE_CHECKING:
     from main import Isabel
 
 LINKS = ["discord.gg/", "discord.com/invite/", "http://", "https://"]
+MESSAGE_LIMIT = 3
 LOGO_BUILDERS_ID = 297657542572507137
 
 
@@ -16,12 +17,13 @@ class MessageCounter:
         self.messages = []
 
     def add_message(self, message: discord.Message):
-        self.messages.append(message)
-        if len(self.messages) > 5:
+        self.messages.append(message.id)
+        if len(self.messages) > MESSAGE_LIMIT:
             self.messages.pop(0)
 
     def clear_old_messages(self):
-        self.messages = [m for m in self.messages if m.created_at > discord.utils.utcnow() - datetime.timedelta(days=7)]
+        now = discord.utils.utcnow()
+        self.messages = [m for m in self.messages if now - discord.utils.snowflake_time(m) < datetime.timedelta(days=7)]
 
     def __len__(self):
         return len(self.messages)
@@ -30,7 +32,16 @@ class MessageCounter:
 class AntiLinksCog(commands.Cog):
     def __init__(self, bot: 'Isabel'):
         self.bot = bot
-        self.activity = {}
+        self.counters = {}
+
+    async def load_channels(self):
+        await self.bot.wait_until_ready()
+        guild = self.bot.get_guild(LOGO_BUILDERS_ID)
+        if guild is None:
+            return
+        for channel in guild.text_channels:
+            async for message in channel.history(limit=100, after=discord.utils.utcnow() - datetime.timedelta(days=7)):
+                self.counters.setdefault(message.author.id, MessageCounter()).add_message(message)
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
@@ -41,18 +52,21 @@ class AntiLinksCog(commands.Cog):
         if message.author.guild_permissions.manage_messages:
             return
 
-        self.activity.setdefault(message.author.id, MessageCounter()).clear_old_messages()
+        self.counters.setdefault(message.author.id, MessageCounter()).clear_old_messages()
+        not_active = len(self.counters.get(message.author.id, [])) < MESSAGE_LIMIT
 
-        if any(link in message.content for link in LINKS) and len(self.activity.get(message.author.id, [])) < 5:
+        if any(link in message.content for link in LINKS) and not_active:
             await message.delete()
             await message.channel.send(f"{message.author.mention}, you are not high enough level to send links here.")
             # that's a joke, we don't actually have levels
-        elif (message.attachments or message.embeds) and len(self.activity.get(message.author.id, [])) < 5:
+        elif (message.attachments or message.embeds) and not_active:
             await message.delete()
             await message.channel.send(f"{message.author.mention}, you are not high enough level to send files here.")
         else:
-            self.activity.setdefault(message.author.id, MessageCounter()).add_message(message)
+            self.counters.setdefault(message.author.id, MessageCounter()).add_message(message)
 
 
 async def setup(bot: 'Isabel'):
-    await bot.add_cog(AntiLinksCog(bot))
+    alc = AntiLinksCog(bot)
+    await alc.load_channels()
+    await bot.add_cog(alc)
