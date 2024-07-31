6f41977c898a4f1c3a6a18e58044070ef8f3bc4a
LittleEndu <enduslittlecomputer@gmail.com>
2024-07-31 08:25:39 +0300

    Rename class and remove magic function

--- extensions/anti_links.py
+++ extensions/anti_links.py
@@ -17,9 +17,10 @@ LOGO_BUILDERS_ID = 297657542572507137
 ROLE_ID = 1230732817550278686
 
 
-class MessageCounter:
-    def __init__(self):
-        self.messages = []
+class MessageCollection:
+    def __init__(self, init_as_full=False):
+        now = discord.utils.time_snowflake(discord.utils.utcnow())
+        self.messages = [now] * MESSAGE_LIMIT if init_as_full else []
 
     def add_message(self, message: discord.Message):
         self.messages.append(message.id)
@@ -30,22 +31,20 @@ class MessageCounter:
         now = discord.utils.utcnow()
         self.messages = [m for m in self.messages if now - discord.utils.snowflake_time(m) < datetime.timedelta(days=7)]
 
-    def __len__(self):
-        return len(self.messages)
+    def is_full(self):
+        return len(self.messages) == MESSAGE_LIMIT
 
 
 class AntiLinksCog(commands.Cog):
     def __init__(self, bot: 'Isabel'):
         self.bot = bot
-        self.counters: dict[int, MessageCounter] = {}
+        self.collectors: dict[int, MessageCollection] = {}
 
         if Path("anti_links.json").exists():
             with open("anti_links.json", "r") as f:
                 data = json.load(f)
-                now = discord.utils.time_snowflake(discord.utils.utcnow())
                 for user_id in data:
-                    self.counters[int(user_id)] = MessageCounter()
-                    self.counters[int(user_id)].messages = [now] * MESSAGE_LIMIT
+                    self.collectors[int(user_id)] = MessageCollection(True)
 
         self.hourly.start()
 
@@ -57,8 +56,8 @@ class AntiLinksCog(commands.Cog):
     async def hourly(self):
         guild = self.bot.get_guild(LOGO_BUILDERS_ID)
         role = guild.get_role(ROLE_ID)
-        for key in list(self.counters):
-            counter = self.counters[key]
+        for key in list(self.collectors):
+            counter = self.collectors[key]
             counter.clear_old_messages()
             if not counter.messages:
                 if guild and role:
@@ -66,13 +65,13 @@ class AntiLinksCog(commands.Cog):
                     if member and role in member.roles:
                         with contextlib.suppress(discord.HTTPException):
                             await member.remove_roles(role, reason="Not active in chat anymore.")
-                del self.counters[key]
+                del self.collectors[key]
             await asyncio.sleep(0)
         self.save_to_json()
 
     def save_to_json(self):
         with open("anti_links.json", "w") as f:
-            json.dump([str(k) for k, v in self.counters.items() if len(v.messages) == MESSAGE_LIMIT], f)
+            json.dump([str(k) for k, v in self.collectors.items() if v.is_full()], f)
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
@@ -85,8 +84,8 @@ class AntiLinksCog(commands.Cog):
         if message.type != discord.MessageType.default:
             return
 
-        self.counters.setdefault(message.author.id, MessageCounter()).clear_old_messages()
-        not_active = len(self.counters.get(message.author.id, [])) < MESSAGE_LIMIT
+        self.collectors.setdefault(message.author.id, MessageCollection()).clear_old_messages()
+        not_active = not self.collectors[message.author.id].is_full()
 
         # sourcery skip: remove-pass-elif
         if any(link in message.content for link in LINKS) and not_active:
@@ -94,8 +93,8 @@ class AntiLinksCog(commands.Cog):
         elif (message.attachments or message.embeds) and not_active:
             pass
         else:
-            self.counters.setdefault(message.author.id, MessageCounter()).add_message(message)
-            if len(self.counters[message.author.id]) == MESSAGE_LIMIT:
+            self.collectors.setdefault(message.author.id, MessageCollection()).add_message(message)
+            if self.collectors[message.author.id].is_full():
                 if guild := self.bot.get_guild(LOGO_BUILDERS_ID):
                     member = guild.get_member(message.author.id)
                     role = guild.get_role(ROLE_ID)
