70317badc51427284079f214cd084c922fe86d05
LittleEndu <enduslittlecomputer@gmail.com>
2025-03-05 11:52:33 +0200

    Make it so reloading cog is unnecessary

--- extensions/thread_per_image.py
+++ extensions/thread_per_image.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 
 from extensions.starboard import IMAGE_URL_REGEX
 
-CHANNEL_IDS = (1221887700890816583, 1286279551898615971)
+CHANNEL_IDS = (1221887700890816583, 1286279551898615971, 1346680639168057344)
 
 if TYPE_CHECKING:
     from main import Isabel
@@ -14,11 +14,10 @@ if TYPE_CHECKING:
 class ThreadPerImageCog(commands.Cog):
     def __init__(self, bot: 'Isabel'):
         self.bot = bot
-        self.channel_ids = bot.config.get('thread_channel_ids', CHANNEL_IDS)
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        if message.channel.id not in self.channel_ids:
+        if message.channel.id not in self.bot.config.get('thread_channel_ids', CHANNEL_IDS):
             return
 
         has_image_attachment = any(attachment.content_type.startswith("image") for attachment in message.attachments)
