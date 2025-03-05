f1212940701f4e5a66a5eedb3fe2b8638a0babd0
LittleEndu <enduslittlecomputer@gmail.com>
2025-03-05 11:49:25 +0200

    Making this a config option has been considered

--- extensions/thread_per_image.py
+++ extensions/thread_per_image.py
@@ -5,19 +5,20 @@ from discord.ext import commands
 
 from extensions.starboard import IMAGE_URL_REGEX
 
-CHANNEL_IDS = (1221887700890816583, 1286279551898615971)  # TODO: consider making this a config option
+CHANNEL_IDS = (1221887700890816583, 1286279551898615971)
 
 if TYPE_CHECKING:
     from main import Isabel
 
 
 class ThreadPerImageCog(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: 'Isabel'):
         self.bot = bot
+        self.channel_ids = bot.config.get('thread_channel_ids', CHANNEL_IDS)
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        if message.channel.id not in CHANNEL_IDS:
+        if message.channel.id not in self.channel_ids:
             return
 
         has_image_attachment = any(attachment.content_type.startswith("image") for attachment in message.attachments)
